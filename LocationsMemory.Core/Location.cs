6b9c48f535d06fc84f01d432ee653e794120ae5e
Cedric Klug-Offermann <cklug@byte5.de>
2019-08-12 13:05:32 +0200

    Projektdateien hinzufügen.

---
+++ LocationsMemory.Core/Location.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocationsMemory.Core
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ LocationsMemory.Data/ILocationsMemory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using LocationsMemory.Core;
+using System.Text;
+using Microsoft.CodeAnalysis;
+
+namespace LocationsMemory.Data
+{
+    public interface ILocationsMemory
+    {
+        IEnumerable<Location> GetAll();
+    }
+
+    public class InMemoryLocationsData : ILocationsMemory
+    {
+        List<Location> locations;
+
+        public InMemoryLocationsData()
+        {
+            locations = new List<Location>()
+            {
+                new Location { Id = 1, Name = "Byte5", Address = "Frankfurt, Hanauerlandstraße 114"}
+            };
+        }
+
+        public IEnumerable<Location> GetAll()
+        {
+
+        }
+    }
+}
